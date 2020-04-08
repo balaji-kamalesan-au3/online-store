cffbc760f2562eb8378a97027f0a1622b78a0164
attainu-instructor/balaji-kamalesan-au3 <balajikamalesan@gmail.com>
2020-04-08 20:00:01 +0530

    Added Sample Data and intialised react app

---
+++ online-store-app/Data/sampledata.js
@@ -0,0 +1,97 @@
+export const laptops = [
+    {
+        Name :  "Mac Book Pro",
+        Manufacturer : "Apple",
+        Image : "https://mk0beetstechqtgw52p3.kinstacdn.com/wp-content/uploads/MacBook-Pro-Unibody-13-10.6-Snow-Leopard-800x800-1.png",
+
+        Description : "Apple MacBook Pro is a macOS laptop with a 13.30-inch display that has a resolution of 2560x1600 pixels. It is powered by a Core i5 processor and it comes with 12GB of RAM. The Apple MacBook Pro packs 512GB of SSD storage.",
+        Price : "50000",
+        Seller : "Cloud Tail",
+    },
+    {
+        Name :  "Dell Latitude",
+        Manufacturer : "Dell",
+        Image : "https://images-na.ssl-images-amazon.com/images/I/814RQS3vkvL._SL1500_.jpg",
+        Description : "Dell XPS 13 9380 13.3  Notebook Computer, Intel Core i7-8565U, 8GB RAM, 256GB SSD, Windows 10 Home, Platinum Silver/Carbon Fiber Black",
+        Price : "40000",
+        Seller : "Cloud Tail",
+        
+    },
+    {
+        Name :  "Lenova Think Pad",
+        Manufacturer : "Lenovo",
+        Image : "https://images-na.ssl-images-amazon.com/images/I/515izYLiyGL._SL1000_.jpg",
+        Description : "Lenovo ThinkPad E480 20KN0041US 14 LCD Notebook - Intel Core i7 (8th Gen) i7-8550U Quad-core (4 Core) 1.80 GHz - 8 GB DDR4 SDRAM - 500 GB HDD - Windows 10 Pro 64-bit (English) - 1920 x 1080 - in",
+        Price : "45000",
+        Seller : "Cloud Tail",
+        
+    },
+    {
+        Name :  "HP Elite Book",
+        Manufacturer : "HP",
+        Image : "https://images-na.ssl-images-amazon.com/images/I/91UIgRI%2BajL._SL1500_.jpg",
+        Description : "HP EliteBook x360 830 G6 13.3 Touchscreen 2 in 1 Notebook - 1920 x 1080 - Core i7 i7-8665U - 16 GB RAM - 32 GB Optane Memory - 256 GB SSD - Windows 10 Pro 64-bit - Intel UHD Graphics 620 - in-pl",
+        Price : "42000",
+        Seller : "Cloud Tail",
+        
+    },
+    {
+        Name :  "Dell Alienware",
+        Manufacturer : "Dell",
+        Image : "https://images-na.ssl-images-amazon.com/images/I/91UIgRI%2BajL._SL1500_.jpg",
+        Description : "2020 Dell Alienware 15 R4 15.6 FHD VR Ready Gaming Laptop Computer, 8th Gen Intel Hexa-Core i7-8750H, 16GB DDR4, 1TB HDD + 256GB PCIe SSD, GeForce GTX 1060, Windows 10 + EST 500GB External Hard Drive",
+        Price : "70000",
+        Seller : "Cloud Tail",
+        
+    }
+
+]
+
+export const mobiles = [
+    {
+        Name :  "Mac Book Pro",
+        Manufacturer : "Apple",
+        Image : "https://mk0beetstechqtgw52p3.kinstacdn.com/wp-content/uploads/MacBook-Pro-Unibody-13-10.6-Snow-Leopard-800x800-1.png",
+
+        Description : "Apple MacBook Pro is a macOS laptop with a 13.30-inch display that has a resolution of 2560x1600 pixels. It is powered by a Core i5 processor and it comes with 12GB of RAM. The Apple MacBook Pro packs 512GB of SSD storage.",
+        Price : "50000",
+        Seller : "Cloud Tail",
+    },
+    {
+        Name :  "Dell Latitude",
+        Manufacturer : "Dell",
+        Image : "https://images-na.ssl-images-amazon.com/images/I/814RQS3vkvL._SL1500_.jpg",
+        Description : "Dell XPS 13 9380 13.3  Notebook Computer, Intel Core i7-8565U, 8GB RAM, 256GB SSD, Windows 10 Home, Platinum Silver/Carbon Fiber Black",
+        Price : "40000",
+        Seller : "Cloud Tail",
+        
+    },
+    {
+        Name :  "Lenova Think Pad",
+        Manufacturer : "Lenovo",
+        Image : "https://images-na.ssl-images-amazon.com/images/I/515izYLiyGL._SL1000_.jpg",
+        Description : "Lenovo ThinkPad E480 20KN0041US 14 LCD Notebook - Intel Core i7 (8th Gen) i7-8550U Quad-core (4 Core) 1.80 GHz - 8 GB DDR4 SDRAM - 500 GB HDD - Windows 10 Pro 64-bit (English) - 1920 x 1080 - in",
+        Price : "45000",
+        Seller : "Cloud Tail",
+        
+    },
+    {
+        Name :  "HP Elite Book",
+        Manufacturer : "HP",
+        Image : "https://images-na.ssl-images-amazon.com/images/I/91UIgRI%2BajL._SL1500_.jpg",
+        Description : "HP EliteBook x360 830 G6 13.3 Touchscreen 2 in 1 Notebook - 1920 x 1080 - Core i7 i7-8665U - 16 GB RAM - 32 GB Optane Memory - 256 GB SSD - Windows 10 Pro 64-bit - Intel UHD Graphics 620 - in-pl",
+        Price : "42000",
+        Seller : "Cloud Tail",
+        
+    },
+    {
+        Name :  "Dell Alienware",
+        Manufacturer : "Dell",
+        Image : "https://images-na.ssl-images-amazon.com/images/I/91UIgRI%2BajL._SL1500_.jpg",
+        Description : "2020 Dell Alienware 15 R4 15.6 FHD VR Ready Gaming Laptop Computer, 8th Gen Intel Hexa-Core i7-8750H, 16GB DDR4, 1TB HDD + 256GB PCIe SSD, GeForce GTX 1060, Windows 10 + EST 500GB External Hard Drive",
+        Price : "70000",
+        Seller : "Cloud Tail",
+        
+    }
+
+]
